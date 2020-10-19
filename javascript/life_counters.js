e095fe3e599c033f45560a50961db9db406cc88e
TDSOJohn <giovanni.basso@icloud.com>
2020-10-19 19:38:22 +0200

    a

--- javascript/life_counters.js
+++ javascript/life_counters.js
@@ -1,6 +1,4 @@
 
-import { getQueryVariable } from 'http://utilities.js';
-
 const baseURL       = 'http://192.168.1.110';
 const templ_path    = '/github/mtg_helper/html/';
 
@@ -23,6 +21,36 @@ const callAPI       = async (myURL) =>
     const response  = await fetch(myURL);
     file            = await response.text();
     return file;
+}
+
+//  client_side utilities
+
+//  Sanitizes string_in checking for ints. Warning: 11fxoifS => 11, a11b => NaN
+function intParser(string_in)
+{
+    const parsed = parseInt(string_in, 10);
+    if(isNaN(parsed) && (parsed !== null))
+    {
+        return 1;
+    }
+    return parsed;
+}
+
+//  Simple utility that returns a query with type specified in "variable"
+function getQueryVariable(variable_in)
+{
+    var query   = window.location.search.substring(1);
+    var vars    = query.split('&');
+    for (var i  = 0; i < vars.length; i++)
+    {
+        var pair= vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable_in)
+        {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable_in);
+    return 0;
 }
 
 //  Player class containing life, rgb value and DOM update methods and values
