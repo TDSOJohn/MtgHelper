fe16fd9b3a5b49f7b1f438e0c7144e4f8f00ed71
ARtbibaOfficial <giovanni.basso@icloud.com>
2023-07-04 10:34:54 +0200

    preparing for github pages deployment

--- javascript/life_counters.js
+++ javascript/life_counters.js
@@ -1,10 +1,4 @@
-
-const baseURL       = 'http://127.0.0.1:8080';
-const templ_path    = '/html/';
-
-var players         = [];
 var domparser       = new DOMParser();
-var xhr             = new XMLHttpRequest();
 
 var main_content    = document.querySelector('.main_content');
 var player_columns;
@@ -13,15 +7,6 @@ var life_buttons_p5;
 var life_buttons_p1;
 var life_buttons_m1;
 var life_buttons_m5;
-
-const callAPI       = async (myURL) =>
-{
-    let file        = "";
-
-    const response  = await fetch(myURL);
-    file            = await response.text();
-    return file;
-}
 
 //  client_side utilities
 
@@ -34,23 +19,6 @@ function intParser(string_in)
         return 1;
     }
     return parsed;
-}
-
-//  Simple utility that returns a query with type specified in "variable"
-function getQueryVariable(variable_in)
-{
-    var query   = window.location.search.substring(1);
-    var vars    = query.split('&');
-    for (var i  = 0; i < vars.length; i++)
-    {
-        var pair= vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable_in)
-        {
-            return decodeURIComponent(pair[1]);
-        }
-    }
-    console.log('Query variable %s not found', variable_in);
-    return 0;
 }
 
 //  Player class containing life, rgb value and DOM update methods and values
