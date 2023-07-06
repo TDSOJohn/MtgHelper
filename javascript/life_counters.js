e158c32010a7b289340037176c8adacc9de22069
ARtbibaOfficial <giovanni.basso@icloud.com>
2023-07-06 17:59:44 +0200

    fixed life_counter. only 2 players and 20 starting life for now

--- javascript/life_counters.js
+++ javascript/life_counters.js
@@ -1,6 +1,7 @@
 var domparser       = new DOMParser();
 
 var main_content    = document.querySelector('.main_content');
+var players = [];
 var player_columns;
 var life_counts;
 var life_buttons_p5;
@@ -11,19 +12,16 @@ var life_buttons_m5;
 //  client_side utilities
 
 //  Sanitizes string_in checking for ints. Warning: 11fxoifS => 11, a11b => NaN
-function intParser(string_in)
-{
+function intParser(string_in) {
     const parsed = parseInt(string_in, 10);
-    if(isNaN(parsed) && (parsed !== null))
-    {
+    if(isNaN(parsed) && (parsed !== null)) {
         return 1;
     }
     return parsed;
 }
 
 //  Player class containing life, rgb value and DOM update methods and values
-function Player(div_in, counter_in)
-{
+function Player(div_in, counter_in) {
     this.life       = 20;
     this.rgb_code   = "";
     this.div        = div_in;
@@ -41,33 +39,34 @@ function Player(div_in, counter_in)
     };
 }
 
-function startup()
-{
+function addPlayer(div_in, counter_in) {
+    let temp_player = new Player(div_in, counter_in);
+    players.push(temp_player);
+}
+
+function startup() {
+    player_columns  = document.querySelectorAll('.column');
+    life_counts     = document.querySelectorAll('.player_life');
+
+    for(let i = 0; i < 2; i++) {
+        addPlayer(player_columns[i], life_counts[i]);
+        players[i].updateRgb();
+    }
+
     life_buttons_p5 = document.querySelectorAll('.button_p5');
     life_buttons_p1 = document.querySelectorAll('.button_p1');
     life_buttons_m1 = document.querySelectorAll('.button_m1');
     life_buttons_m5 = document.querySelectorAll('.button_m5');
 
-    let i = 0;
-    for(const button of life_buttons_p5) {
-        button.addEventListener('click', function() { players[i].hit(5); });
-        i++;
-    }
-    i = 0;
-    for(const button of life_buttons_p1) {
-        button.addEventListener('click', function() { players[i].hit(1); });
-        i++;
-    }
-    i = 0;
-    for(const button of life_buttons_m1) {
-        button.addEventListener('click', function() { players[i].hit(-1); });
-        i++;
-    }
-    i = 0;
-    for(const button of life_buttons_m5) {
-        button.addEventListener('click', function() { players[i].hit(-5); });
-        i++;
-    }
+    // is there a better option? not really elegante...
+    life_buttons_p5[0].addEventListener('click', function() { players[0].hit(5); });
+    life_buttons_p5[1].addEventListener('click', function() { players[1].hit(5); });
+    life_buttons_p1[0].addEventListener('click', function() { players[0].hit(1); });
+    life_buttons_p1[1].addEventListener('click', function() { players[1].hit(1); });
+    life_buttons_m1[0].addEventListener('click', function() { players[0].hit(-1); });
+    life_buttons_m1[1].addEventListener('click', function() { players[1].hit(-1); });
+    life_buttons_m5[0].addEventListener('click', function() { players[0].hit(-5); });
+    life_buttons_m5[1].addEventListener('click', function() { players[1].hit(-5); });
 
 }
 
